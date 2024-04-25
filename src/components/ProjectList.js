9e176a7c8603302cfaad73ec933b94690946bde1
Mwenda <kelvinfotech@gmail.com>
2024-04-25 13:05:37 +0300

    React Hooks Lists And Keys Lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,16 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project) => (
+          <ProjectItem
+            key={project.id} // Use the id of the project for the key prop
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies}
+          />
+        ))}
+      </div>
     </div>
   );
 }
